b3ed9fa6319c2f6cd3126a62aec6dca4d1720e6e
DESKTOP-50KIK69\phu <mchoang98@gmail.com>
2021-01-16 10:42:53 +0700

    update circle ci

---
+++ Test Suites/NewTestSuite.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ca58f0c7-17a0-4583-aa9d-8cd7d8b84ce4</testSuiteGuid>
+   <testCaseLink>
+      <guid>fbd03ca9-6f4a-4706-bb00-4e0859bdfe34</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginAndRegistration Screen/SGBTestRegistrationSuccess</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5382e5e-3280-4b85-b77c-986710679ce0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginAndRegistration Screen/SGBTestLoginSuccess</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>97add24d-721b-45d8-83f6-1f352e2101bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/e-Training/ET Main Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b1af34f-ec8f-4cb4-997d-e0c5fe255953</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/Court/Court Main Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>05976a42-be9e-4ce8-af21-6a9b2acf7bda</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/Academy/AcademyUploadImage/Upload Photo Toan</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>61871b87-b0a6-406c-851b-21639f5bf5c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/Academy/AcademyUploadImage/Upload Photo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44791452-9932-482a-987a-d03fba8112a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/Academy/AcademyUploadImage/CropImage</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cf35010b-f2ad-4322-920b-b0c24d47e967</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/Academy/AcademyUploadImage/ChangeMobileArea</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>579408d9-8b96-490d-a3b0-f76f11f0f756</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/Academy/AcademyUploadImage/ChangeEcMobileArea</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>72d63580-b27f-43f4-a45d-dbf888fe87fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu Screen/Academy/Aca Main Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
